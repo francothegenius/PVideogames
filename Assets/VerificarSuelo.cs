ec31b31a5ebc6db1627a0ac694f4a103500450f1
Francisco The Genius <franciscothegenius@Franciscos-MacBook-Pro.local>
2020-04-07 14:18:14 -0500

    Avance proyecto

---
+++ Assets/VerificarSuelo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerificarSuelo : MonoBehaviour
+{
+    private player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponentInParent<player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Suelo") {
+            player.pisando = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Suelo")
+        {
+            player.pisando = false;
+        }
+    }
+
+}
---
+++ Assets/player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float maxSpeed = 5f;
+    public float speed = 2f;
+    public bool pisando;
+    public float JumpForce = 6.5f;
+    private Rigidbody2D rb;
+    private Animator animator;
+    private bool jump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+        animator.SetBool("Pisando", pisando);
+        float h = Input.GetAxis("Horizontal");
+        rb.AddForce(Vector2.right * speed * h);
+        // Debug.Log(rb.velocity.x);
+        float limitSpeed = Mathf.Clamp(rb.velocity.x,-maxSpeed,maxSpeed);
+        rb.velocity = new Vector2(limitSpeed, rb.velocity.y);
+
+        if (h>0.1f) {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        if (h < -0.1f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && pisando) {
+            jump = true;
+        }
+        if (jump) {
+            rb.AddForce(Vector2.up *JumpForce, ForceMode2D.Impulse);
+            jump = false;
+        }
+    }
+
+
+}
