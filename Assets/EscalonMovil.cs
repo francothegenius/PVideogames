a44988be77225cc7f6d28bc45132aaa211ab3cf0
José Adolfo <jmicalco@gmail.com>
2020-04-08 13:06:53 -0500

    Plataforma movil

---
+++ Assets/EscalonMovil.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscalonMovil : MonoBehaviour
+{
+    public Transform objetivo;
+    public float velocidad;
+    private Vector3 inicio, fin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (objetivo != null)
+        {
+            objetivo.parent = null;
+            inicio = transform.position;
+            fin = objetivo.position;
+        }   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (objetivo != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, objetivo.position, velocidad * Time.deltaTime);
+        }
+
+        if (transform.position == objetivo.position)
+        {
+            objetivo.position = (objetivo.position == inicio) ? fin : inicio;
+        }
+    }
+}
--- Assets/VerificarSuelo.cs
+++ Assets/VerificarSuelo.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class VerificarSuelo : MonoBehaviour
 {
     private player player;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponentInParent<player>();
+        rb = GetComponentInParent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -17,17 +19,39 @@ public class VerificarSuelo : MonoBehaviour
         
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "SueloMovil")
+        {
+            rb.velocity = new Vector3(0f, 0f, 0f);
+            player.transform.parent = collision.transform;
+            player.pisando = true;
+        }
+    }
+
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Suelo") {
             player.pisando = true;
         }
+
+        if (collision.gameObject.tag == "SueloMovil")
+        {
+            player.transform.parent = collision.transform;
+            player.pisando = true;
+        }
+
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Suelo")
         {
+            player.pisando = false;
+        }
+        if (collision.gameObject.tag == "SueloMovil")
+        {
+            player.transform.parent = null;
             player.pisando = false;
         }
     }
