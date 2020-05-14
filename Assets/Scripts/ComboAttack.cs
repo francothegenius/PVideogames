e1c024f95bf917297bc69f1268be0aac545029a7
francothegenius <A01634495@itesm.mx>
2020-05-14 15:37:32 -0500

    minor fixes

---
+++ Assets/Scripts/ComboAttack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComboAttack : MonoBehaviour
+{
+    public Image barra;
+    public float progreso , maxProgreso;
+    // Start is called before the first frame update
+    void Start()
+    {
+        progreso = 0f;
+        maxProgreso = 100f;
+    }
+
+    public void subirProgreso(float cantidad) 
+    {
+        progreso = Mathf.Clamp(progreso+cantidad,0f, maxProgreso);
+        barra.transform.localScale = new Vector2(vida/maxVida, 1);
+    }
+}
